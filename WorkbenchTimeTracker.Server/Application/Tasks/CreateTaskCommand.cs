f8cc9851689d2ab918bf0e587f26c6756b67ecdb
Vašek Henzl <vasek.henzl@gmail.com>
2025-07-02 11:35:25 +1200

    Add "create task" command

---
+++ WorkbenchTimeTracker.Server/Application/Tasks/CreateTaskCommand.cs
@@ -0,0 +1,10 @@
+﻿using WorkbenchTimeTracker.Server.Application.BuildingBlocks;
+
+namespace WorkbenchTimeTracker.Server.Application.Tasks
+{
+    public record CreateTaskCommand(
+        string Title,
+        string? Description,
+        Guid? AssigneeId
+    ) : ICommand<Guid>;
+}
---
+++ WorkbenchTimeTracker.Server/Application/Tasks/CreateTaskCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using WorkbenchTimeTracker.Server.Application.BuildingBlocks;
+using WorkbenchTimeTracker.Server.Domain;
+
+namespace WorkbenchTimeTracker.Server.Application.Tasks
+{
+    public class CreateTaskCommandHandler : ICommandHandler<CreateTaskCommand, Guid>
+    {
+        private readonly ITaskRepository _taskRepository;
+        private readonly IPersonRepository _personRepository;
+
+        public CreateTaskCommandHandler(ITaskRepository taskRepository, IPersonRepository personRepository)
+        {
+            _taskRepository = taskRepository;
+            _personRepository = personRepository;
+        }
+
+        public async Task<Guid> HandleAsync(CreateTaskCommand command)
+        {
+            Person? assignee = null;
+            if (command.AssigneeId.HasValue)
+                assignee = await _personRepository.GetAsync(command.AssigneeId.Value);
+
+            var task = Domain.Task.Create(
+                command.Title,
+                command.Description ?? string.Empty,
+                assignee
+            );
+
+            await _taskRepository.AddAsync(task);
+            return task.Id;
+        }
+    }
+}
