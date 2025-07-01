435a29095fc6aa9ccd12be0d8025e0ede3286a43
Vašek Henzl <vasek.henzl@gmail.com>
2025-07-01 16:47:10 +1200

    Install and configure Entity Framework

---
+++ WorkbenchTimeTracker.Server/Infrastructure/Persistence/TimeTrackerDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WorkbenchTimeTracker.Server.Infrastructure.Persistence
+{
+    public class TimeTrackerDbContext : DbContext
+    {
+        public TimeTrackerDbContext(DbContextOptions<TimeTrackerDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- WorkbenchTimeTracker.Server/Program.cs
+++ WorkbenchTimeTracker.Server/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WorkbenchTimeTracker.Server.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<TimeTrackerDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+));
 
 var app = builder.Build();
 
